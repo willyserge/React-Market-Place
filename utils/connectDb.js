0620ae324db199168a9a17f7a45de578194539b2
willyserge <willyserge01@gmail.com>
2020-01-03 14:14:46 +0200

    connect to database

---
+++ utils/connectDb.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async function connectDb(){
+if ( connection.isConnected) {
+     console.log('using existing connection')
+     return;
+}
+
+    const db = await mongoose.connect(process.env.MONGO_SRV,{
+        useCreateIndex:true,
+        useFindAndModify:false,
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    console.log('db connected')
+    connection.isConnected = db.connections[0].readyState;
+}
+export default connectDb;
