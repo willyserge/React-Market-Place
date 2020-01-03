679ff598342913518f6c0e5c4cbc2f19e18114da
willyserge <willyserge01@gmail.com>
2020-01-03 18:37:11 +0200

    fetch product data from the database

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home() {
+import React from 'react'
+import axios from 'axios'
+function Home({ products }) {
+  console.log(products)
   return <>home</>;
+}
+Home.getInitialProps = async () =>{
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return {products: response.data}
+
 }
 
 export default Home;
