0f4d7e7410859b14978788635531db8b225aff39
willyserge <willyserge01@gmail.com>
2020-01-10 18:41:21 +0200

    add state to form and image preview

--- pages/create.js
+++ pages/create.js
@@ -1,29 +1,76 @@
-import { Header, Icon, Form, Input, TextArea, Button } from "semantic-ui-react";
+
+import { Header, Icon, Form, Input, TextArea, Button, Image, Message } from "semantic-ui-react";
+import { useState } from "react";
 
 function CreateProduct() {
+
+  const INITIAL_PRODUCT = {
+    name:'',
+    price:'',
+    media:'',
+    description:''
+  }
+
+  const [product,setProduct] = useState(INITIAL_PRODUCT)
+
+  const [mediaPreview, setMediaPreview] = useState('')
+
+  const [success, setsuccess] = useState(false)
+
+  const handleChange = (e)=>{
+    const { name , value , files} = e.target;
+
+    if (name == 'Media') {
+      setProduct((prevState)=>({
+        ...prevState , media:files[0]
+      }))
+     setMediaPreview(window.URL.createObjectURL(files[0]))
+    }
+    else{
+      setProduct((prevState)=>({
+        ...prevState , [name]:value
+      }))
+    }
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(product)
+    setProduct(INITIAL_PRODUCT)
+    setsuccess(true)
+    
+
+  }
+
   return (
     <>
       <Header as='h2' block>
       <Icon name="add" color="orange"/>
         Create new product
       </Header>
-      <Form>
+      <Form success={success} onSubmit={handleSubmit}>
+
+      <Message success icon="check" header="Success!" content="Your product has been posted"/>
         <Form.Group widths='equal'>
            <Form.Field 
              control= {Input}
-             name="Name"
+             name="name"
              label="Name"
              placeholder="Name"
+             value={product.name}
+             onChange={handleChange}
            />
 
            <Form.Field   
              control= {Input}
-             name="Price"
+             name="price"
              label="Price"
              placeholder="Price"
              min="0.00"
              step="0.01"
              type="number"
+             value={product.price}
+             onChange={handleChange}
            />
            <Form.Field 
              control= {Input}
@@ -32,17 +79,22 @@ function CreateProduct() {
              label="media"
              accept="image/*"
              content="Select Image"
+             onChange={handleChange}
 
            />
 
          
         </Form.Group>
 
+        <Image src={mediaPreview} rounded centered size="small"/>
+
           <Form.Field 
              control= {TextArea}
-             name="Description"
+             name="description"
              label="Description"
              placeholder="Description"
+             value={product.description}
+             onChange={handleChange}
            />
 
           <Form.Field 
