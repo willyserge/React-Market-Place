33678137ea4e320893fdcc7c443ccae43a3b4569
willyserge <willyserge01@gmail.com>
2020-01-18 11:55:32 +0200

    create signup and login pages

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,117 @@
-function Login() {
-  return <>login</>;
-}
+import React, { useState, useEffect } from "react";
+import { Button, Form, Icon, Message, Segment } from "semantic-ui-react";
+import Link from "next/link";
+import axios from "axios";
+
+import catchErrors from "../utils/catchErrors";
+import baseUrl from "../utils/baseUrl";
+import { handleLogin } from "../utils/auth";
+
+const INITIAL_USER = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+
+  const [user, setUser] = useState(INITIAL_USER);
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [error, setError] = useState("");
+
+
+  useEffect(() => {
+    const isUser = Object.values(user).every((element) => {
+      return Boolean(element);
+    });
+    isUser ? setDisabled(false) : setDisabled(true);
+  }, [user]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      setError("");
+
+      const url = `${baseUrl}/api/login`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+
+      handleLogin(response.data);
+      //
+    } catch (error) {
+      catchErrors(error, setError);
+
+      //
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  
+  return (
+    <>
+      <Message
+        attached
+        icon="privacy"
+        header="Welcome back!"
+        content="Login in with email and password"
+        color="blue"
+      />
+      <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+        <Message error header="Oops!" content={error} />
+        <Segment>
+          <Form.Input
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            label="Email"
+            placeholder="Email"
+            name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            label="Password"
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <Button
+            disabled={disabled || loading}
+            icon="sign in"
+            type="submit"
+            color="orange"
+            content="Login"
+          />
+        </Segment>
+      </Form>
+      <Message attached="bottom" warning>
+        <Icon name="help" />
+        New user?{" "}
+        <Link href="/signup">
+          <a>Sign up here</a>
+        </Link>{" "}
+        instead.
+      </Message>
+    </>
+  );
+};
 
 export default Login;
